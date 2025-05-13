3e120425e8e2d9c3a94bc0ec6dcb7b08dca78459
wndrflsoul <wndrflsoul@ya.ru>
2025-05-13 20:27:27 +0700

    feat: add bugsnag

--- app_one/hello.php
+++ app_one/hello.php
@@ -1,2 +1,7 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+$bugsnag = Bugsnag\Client::make('706d75ff9bd8cf901173410ad82b337a');
+Bugsnag\Handler::register($bugsnag);
+$bugsnag->notifyException(new RuntimeException("Test error"));
+
 echo "Hello, world!";
